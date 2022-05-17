ee66ad960fcaec6a702f66711f64b2076ce21339
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-17 12:12:45 +0200

    js

--- JS/Pruebas-JS.js
+++ JS/Pruebas-JS.js
@@ -1,1 +1,15 @@
 console.log("Hello world!, tecto en la consola")
+function Cambia() {
+    document.getElementById("demo").innerHTML = "Segundo texto";
+}
+function Inicial() {
+    document.getElementById("demo").innerHTML = "Primer texto";
+}
+$(document).ready(function(){
+console.log('Librería jQuery cargada');
+});
+console.log('hola');
+console.log('en', 'un', 'lugar', 'de', 'la', 'Mancha');
+console.info('Info');
+console.warn('Warning');
+console.error('Error');
