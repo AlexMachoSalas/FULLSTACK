141afbfcc36aae35e4aa7e0af549b2c0bb0a69b6
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-17 12:45:38 +0200

    variables

---
+++ JS/Variables-JS.js
@@ -0,0 +1,11 @@
+// kebap-case (no permitido en JavaScript)
+// const last-name = 'Doe';
+// snake_case (recomendado en otros lenguajes como Python)
+const last_name = 'last_name';
+// PascalCase (recomendado en JavaScript para las clases)
+const LastName = 'LastName';
+// camelCase (recomendaLastNamedo en JavaScript para las variables y funciones)
+const lastName = 'lastName';
+document.write(last_name);
+document.write(LastName);
+document.write(lastName);
