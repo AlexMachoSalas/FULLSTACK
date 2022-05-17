8ac34bd38034e932a8f44427f4e8fbc30c097315
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-17 13:20:51 +0200

    Update Variables-JS.js

--- JS/Variables-JS.js
+++ JS/Variables-JS.js
@@ -1,11 +1,18 @@
 // kebap-case (no permitido en JavaScript)
 // const last-name = 'Doe';
 // snake_case (recomendado en otros lenguajes como Python)
-const last_name = 'last_name';
+const last_name = ' last_name';
 // PascalCase (recomendado en JavaScript para las clases)
-const LastName = 'LastName';
+const LastName = ' LastName';
 // camelCase (recomendaLastNamedo en JavaScript para las variables y funciones)
-const lastName = 'lastName';
-document.write(last_name);
-document.write(LastName);
-document.write(lastName);
+const lastName = ' lastName';
+const person = ' John Doe', carName = ' Volvo ', price = 200;
+document.write(last_name, LastName, lastName, person, carName, price);
+// x está indefinido (undefined)
+let x;
+// ahora x es un número
+x = 5;
+// ahora x es un string
+x = 'John';
+// Infinity
+const variable1 = 1/0;
