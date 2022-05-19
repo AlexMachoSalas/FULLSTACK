345480a0a70bcc0be9c2613a83b28b50588c3d0a
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-19 10:23:10 +0200

    Update Ejerciciopagina168.js

--- Ejercicios/Ejerciciopagina168.js
+++ Ejercicios/Ejerciciopagina168.js
@@ -27,11 +27,51 @@ else if ((a+b) < (c+d)) {
     document.write("a+b < c+d")
 }
 document.write("<br>")
+document.write("Ejercicio operadores logicos, puesto por el profesor<br>")
+let hora = 14
+if ((hora>=9)&&(hora<=14)) {
+    document.write("Sant Boi")
+}
+if ((hora>=15)&&(hora<=20)) {
+    document.write("Castelldefels")
+}
+if ((hora<9)&&(hora>20)){
+    document.write("No tengo clase")    
+}
+if ((hora<15)&&(hora>14)){
+    document.write("No tengo clase")    
+}
+document.write("<br>")
 document.write("Ejercicio 3<br>")
 angulo1 = 50
-angulo2 = 60
+angulo2 = 70
 angulo3 = 70
 if ((angulo1+angulo2+angulo3)==180) {
     document.write("Crean un triangulo")
+}
+if (((angulo1+angulo2+angulo3)<180)||(angulo1+angulo2+angulo3)>180) {
+    document.write("No crean un triangulo")
+}
+document.write("<br>")
+document.write("Ejercicio 4<br>")
+variable = 17
+if ((variable % 2) == 0) {
+    document.write("Es un numero par")
+} else {
+    document.write("Es un numero impar")
+}
+document.write("<br>")
+document.write("Ejercicio 5<br>")
+num1 = 2
+num2 = 3
+num3 = 4
+if ((num1>num2)&&(num1>num3)) {
+    document.write("num1 es el mayor")
+}
+if ((num2>num1)&&(num2>num3)) {
+    document.write("num2 es el mayor")
+}
+if ((num3>num1)&&(num3>num2)) {
+    document.write("num3 es el mayor")
 }
 document.write("<br>")
