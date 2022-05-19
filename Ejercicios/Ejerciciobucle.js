5a2393c0e42144ae372d07c96206ba3fce321efb
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-19 13:27:27 +0200

    Update Ejerciciobucle.js

--- Ejercicios/Ejerciciobucle.js
+++ Ejercicios/Ejerciciobucle.js
@@ -1,3 +1,13 @@
-for (let i = 1; i < 51; i++) {
-    console.log(i);
+for (let i = 0; i < 11; i++) {
+    document.write("5x" + i + "=" + i*5 + "<br>");
 }
+
+const array=['En','un','lugar','de','la','mancha'];
+const len = array.length;
+    for(let i = 0; i < len; i++) {
+        console.log(array[i]);
+    }
+    const nombre = prompt("Escriba su nombre", "Escriba aquí");
+    for (let i = 0; i < 11; i++) {
+        document.write(nombre + "<br>");
+    }
