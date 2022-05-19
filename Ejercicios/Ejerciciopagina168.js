164c58f6b6eebb703de7d7304e50761c76458c15
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-19 09:51:30 +0200

    Ejercicio pagina 168

---
+++ Ejercicios/Ejerciciopagina168.js
@@ -0,0 +1,37 @@
+document.write("Ejercicio 1 <br>");
+document.write(4.3>=4);
+document.write("<br>");
+document.write(1==2);
+document.write("<br>");
+document.write(4<4);
+document.write("<br>");
+document.write(2!==5);
+document.write("<br>");
+document.write(5=='5');
+document.write("<br>");
+document.write(5==='5');
+document.write("<br>");
+document.write(5==5);
+document.write("<br>");
+document.write(5===5);
+document.write("<br>");
+document.write("Ejercicio 2<br>");
+a = 2
+b = 1
+c = 6
+d = 4
+if ((a+b) > (c+d)) {
+    document.write("a+b > c+d")
+}
+else if ((a+b) < (c+d)) {
+    document.write("a+b < c+d")
+}
+document.write("<br>")
+document.write("Ejercicio 3<br>")
+angulo1 = 50
+angulo2 = 60
+angulo3 = 70
+if ((angulo1+angulo2+angulo3)==180) {
+    document.write("Crean un triangulo")
+}
+document.write("<br>")
