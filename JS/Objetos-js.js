63fd65ec3b30f81c46cf61428c78f92dfa430ce6
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-24 11:14:51 +0200

    Update Objetos-js.js

--- JS/Objetos-js.js
+++ JS/Objetos-js.js
@@ -1,19 +1,30 @@
-const persona = {
+const persona1 = {
     nombre: 'Alejandro',
     'lugar de residencia': 'Madrid'
     };
     // Alejandro
-    console.log(persona.nombre);
+    console.log(persona1.nombre);
     // Alejandro
-    console.log(persona['nombre']);
+    console.log(persona1['nombre']);
     // error
-    //console.log(persona.lugar de residencia);
+    //console.log(persona1.lugar de residencia);
     // en este caso son obligatorios los corchetes
-    persona['lugar de residencia'] = 'Sevilla';
+    persona1['lugar de residencia'] = 'Sevilla';
     // Sevilla
-    console.log(persona['lugar de residencia']);
+    console.log(persona1['lugar de residencia']);
     const propiedad = 'nombre';
-    // undefined porque busca la variable propiedad dentro del objeto persona
-    console.log(persona.propiedad);
+    // undefined porque busca la variable propiedad dentro del objeto persona1
+    console.log(persona1.propiedad);
     // Alejandro
-    console.log(persona[propiedad]);
+    console.log(persona1[propiedad]);
+const persona2 = {
+    nombre: 'Carmen',
+    apellidos: 'González',
+    datos: {
+    dirección: 'Sevilla'
+    }
+    };
+    // Sevilla
+    console.log(persona2.datos.dirección);
+    // undefined
+    console.log(persona2.datos.otrodato);
