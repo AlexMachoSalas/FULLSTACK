faa0a11259b96c7d0dcf224abae7e99d4ab753d8
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-24 09:55:47 +0200

    ejercicio p 212

---
+++ JS/Ejercicio-1-212.js
@@ -0,0 +1,18 @@
+let numeros = [0,1,2,3,4,5]
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
+    
+}
+for (let numero of numeros) {
+    console.log(numero)
+}
+// ejercicio 2
+let colores = ["amarillo","azul","rojo"]
+for (let i = 0; i < colores.length; i++) {
+    console.log(colores[i]);
+    
+}
+let colores1 = []
+for (let color of colores) {
+    colores.push (color)
+}
