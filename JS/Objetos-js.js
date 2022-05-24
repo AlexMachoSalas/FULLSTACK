25abe431f315f55250f4c3d262ed96e9c3be21af
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-24 12:59:13 +0200

    Update Objetos-js.js

--- JS/Objetos-js.js
+++ JS/Objetos-js.js
@@ -46,3 +46,29 @@ const x = {
     fresas: 20
 };
 delete x.fresas;
+// obligatorio utilizar function para posteriomente crear objetos con new
+const Person = function(name) {
+    // atributo público porque está declarado con this
+    this.name = name || '';
+    // atributo privado porque está declarado con const
+    const lastname = 'González';
+    // atributo privado porque está declarado con const
+    const myName = this.name;
+    // método privado porque está declarado con const
+    const fullName = () => {
+    return myName + ' ' + lastname;
+    };
+    // método público porque está declarado con this
+    this.introduce = () => {
+    return 'Hola, mi nombre es ' + fullName();
+    };
+};
+const oscar = new Person('Óscar');
+// Óscar
+console.log(oscar.name);
+// Hola, mi nombre es Óscar González
+console.log(oscar.introduce());
+// undefined porque el atributo es privado
+console.log(oscar.lastname);
+// error porque el método no privado y no se puede acceder a él (es undefined) e invocara undefined genera un error
+console.log(oscar.fullName());
