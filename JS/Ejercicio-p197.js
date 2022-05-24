3addfa17ce9b150db67b22a01edf869555a8bd40
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-24 13:41:15 +0200

    ejercicio

---
+++ JS/Ejercicio-p197.js
@@ -0,0 +1,12 @@
+// Ejercicio 1
+const persona1 = {
+    nombre: 'David Rayy',
+    sclass: 'VI',
+    rollno : 12
+};
+console.log(persona1)
+const persona2 = {};
+persona2.nombre = 'David Rayy';
+persona2.sclass = 'VI';
+persona2.rollno = 12;
+console.log(persona2)
--- JS/Objetos-js.js
+++ JS/Objetos-js.js
@@ -1,22 +1,22 @@
 const persona1 = {
     nombre: 'Alejandro',
     'lugar de residencia': 'Madrid'
-    };
-    // Alejandro
-    console.log(persona1.nombre);
-    // Alejandro
-    console.log(persona1['nombre']);
-    // error
-    //console.log(persona1.lugar de residencia);
-    // en este caso son obligatorios los corchetes
-    persona1['lugar de residencia'] = 'Sevilla';
-    // Sevilla
-    console.log(persona1['lugar de residencia']);
-    const propiedad = 'nombre';
-    // undefined porque busca la variable propiedad dentro del objeto persona1
-    console.log(persona1.propiedad);
-    // Alejandro
-    console.log(persona1[propiedad]);
+};
+// Alejandro
+console.log(persona1.nombre);
+// Alejandro
+console.log(persona1['nombre']);
+// error
+//console.log(persona1.lugar de residencia);
+// en este caso son obligatorios los corchetes
+persona1['lugar de residencia'] = 'Sevilla';
+// Sevilla
+console.log(persona1['lugar de residencia']);
+const propiedad = 'nombre';
+// undefined porque busca la variable propiedad dentro del objeto persona1
+console.log(persona1.propiedad);
+// Alejandro
+console.log(persona1[propiedad]);
 const persona2 = {
     nombre: 'Carmen',
     apellidos: 'González',
