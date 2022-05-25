90a0ce04b7e8af31712092b36694c4116d3954d0
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-25 13:43:44 +0200

    ejercicio p 176

--- JS/Ejercicio-p-176.js
+++ JS/Ejercicio-p-176.js
@@ -6,3 +6,16 @@ console.log(sumar(1,2));
 console.log(restar(5,1));
 console.log(multiplicar(3,2));
 console.log(dividir(4,2));
+const callback = (resultado) => {console.log(resultado)}
+const sumar1 = (a,b,callback) => callback(a + b);
+const restar1 = (a,b,callback) => callback(a - b);
+const multiplicar1 = (a,b,callback) => callback(a * b);
+const dividir1 = (a,b,callback) => callback(a * b);
+sumar1(1,5,callback);
+restar1(5,1,callback);
+multiplicar1(3,2,callback);
+dividir1(4,2,callback);
+sumar1(1, 2, (resultado) => {console.log(resultado);});
+restar1(1, 2, (resultado) => {console.log(resultado);});
+multiplicar1(1, 2, (resultado) => {console.log(resultado);});
+dividir1(1, 2, (resultado) => {console.log(resultado);});
