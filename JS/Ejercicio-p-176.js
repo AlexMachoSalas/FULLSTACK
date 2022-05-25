22349e3dae60343f81c21d34a8c20de7fcf3ac4f
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-25 11:30:16 +0200

    Ejercicio pagina 176

---
+++ JS/Ejercicio-p-176.js
@@ -0,0 +1,8 @@
+const sumar = (a,b) => a + b;
+const restar = (a,b) => a - b;
+const multiplicar = (a,b) => a * b;
+const dividir = (a,b) => a / b;
+console.log(sumar(1,2));
+console.log(restar(5,1));
+console.log(multiplicar(3,2));
+console.log(dividir(4,2));
