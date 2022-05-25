6919848ed927bbf3fd5b3f30b56dbebfd3146b51
AlexMachoSalas <105197774+AlexMachoSalas@users.noreply.github.com>
2022-05-25 09:22:50 +0200

    Update Ejercicio-p197.js

--- JS/Ejercicio-p197.js
+++ JS/Ejercicio-p197.js
@@ -9,5 +9,29 @@ const banco1 = {
         console.log("Su tranferencia ha sido realizada por el gestor " + this.gestor)
     }
 }
+const banco2 = {
+    gestor: "25",
+    cliente: "105",
+    mensaje(){
+        console.log("Hola cliente " + this.cliente)
+    },
+    trasferencia(){
+        console.log("Su tranferencia ha sido realizada por el gestor " + this.gestor)
+    }
+}
+const banco3 = {
+    gestor: "12",
+    cliente: "30",
+    mensaje(){
+        console.log("Hola cliente " + this.cliente)
+    },
+    trasferencia(){
+        console.log("Su tranferencia ha sido realizada por el gestor " + this.gestor)
+    }
+}
 banco1.mensaje()
 banco1.trasferencia()
+banco2.mensaje()
+banco2.trasferencia()
+banco3.mensaje()
+banco3.trasferencia()
